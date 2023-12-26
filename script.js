c2545e56dd60b392dfb7dfbb7584f5d218af7fc1
Md-Shahzadah-Sattar <148210694+Md-Shahzadah-Sattar@users.noreply.github.com>
2023-12-26 18:07:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 function validEmail(str) {
-  //your JS code here.
+  // Define a regular expression for a basic email validation
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+  // Check if the email matches the regular expression
+  return emailRegex.test(str);
 }
 
-// Do not change the code below.
-const str = prompt("Enter an email address.");
-alert(validEmail(str));
+// Uncomment the code below to prompt the user for an email address and display the result.
+// const str = prompt("Enter an email address.");
+// alert(validEmail(str));
